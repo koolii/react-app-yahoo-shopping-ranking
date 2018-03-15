878ab022ef408f73afc2e12e0556faccd5e7cfe4
koolii <kuriyama@createentrance.com>
2018-03-15 08:04:45 +0100

    :+1: add basic action jest test

---
+++ src/actions/Task.js
@@ -0,0 +1,8 @@
+const addTask = (task) => ({
+  type: 'ADD_TASK',
+  payload: { task },
+})
+
+export default {
+  addTask,
+}
---
+++ src/actions/Task.test.js
@@ -0,0 +1,13 @@
+import actions from './Task'
+
+describe('Action - Task', () => {
+  it('addTask', () => {
+    const task = 'dummy task'
+    const result = actions.addTask(task)
+    const expected = {
+      type: 'ADD_TASK',
+      payload: { task },
+    }
+    expect(result).toEqual(expected)
+  })
+})
